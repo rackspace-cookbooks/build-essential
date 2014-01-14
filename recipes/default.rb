f364a26ff926191e5243b1949056473e7c732d60
schplat <schplat@gmail.com>
2014-01-14 11:53:45 -0600

    edit build_essential to rackspace_build_essential

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: build-essential
+# Cookbook Name:: rackspace_build_essential
 # Recipe:: default
 #
 # Copyright 2008-2009, Opscode, Inc.
@@ -19,7 +19,7 @@
 #
 
 begin
-  include_recipe "build-essential::#{node['platform_family']}"
+  include_recipe "rackspace_build_essential::#{node['platform_family']}"
 rescue Chef::Exceptions::RecipeNotFound
   Chef::Log.warn "A build-essential recipe does not exist for the platform_family: #{node['platform_family']}"
 end
