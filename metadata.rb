75dda90027d5ab19871d6b5adbe88a8ed6e49b49
schplat <schplat@gmail.com>
2014-01-14 11:48:47 -0600

    Update metadata.rb
    Changed - to _

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name              "build-essential"
+name              "rackspace_build_essential"
 maintainer        "Rackspace, US Inc."
 maintainer_email  "rackspace-cookbooks@rackspace.com"
 license           "Apache 2.0"
 description       "Installs C compiler / build tools"
 version           "2.0.0"
-recipe            "build-essential", "Installs packages required for compiling C software from source."
+recipe            "rackspace_build_essential", "Installs packages required for compiling C software from source."
 
 %w{ fedora redhat centos ubuntu debian }.each do |os|
   supports os
